ed2eea7680b7050236839d86b4ccbacf4d5df7e8
Debjyoti Das <debjyotid.2706@gmail.com>
2022-09-02 02:56:20 +0530

    Added fucntion arrayToString and searchAndReplace

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,39 @@ public class Main {
         System.out.println(arraySearch(array,99));
         System.out.println(arraySearch(array,909));
         System.out.println(arraySearch(emptyArray,909));
+
         System.out.println(arraySearchWithValue(emptyArray,909));
         System.out.println(arraySearchWithValue(array,909));
         System.out.println(arraySearchWithValue(array,99));
         System.out.println(arraySearchWithValue(array,44));
 
+        System.out.println(arrayToString(array));
+
+        System.out.println(arrayToString(arraySearchAndReplace(array,44, 0)));
+
+        System.out.println(arrayToString(array));
+
 
     }
 
+    static String arrayToString(int array[]) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[ ");
+        for(int i = 0; i < array.length; i++) {
+            stringBuilder.append(array[i]);
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+    static int[] arraySearchAndReplace(int array[] , int from, int to) {
+        for(int idx = 0; idx < array.length; idx++) {
+            if(array[idx] == from) {
+                array[idx] = to;
+            }
+        }
+        return array;
+    }
 
     static boolean arraySearch(int array[], int value ) {
         for(int idx = 0; idx < array.length; idx++) {
